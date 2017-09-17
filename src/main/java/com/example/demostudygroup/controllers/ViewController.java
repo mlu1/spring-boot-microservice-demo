21e6967987528ef1fdb4ce05e886cbba779cc690
mlu1 <mthandemeluleki@yahoo.com>
2017-09-17 22:30:06 +0800

    View Controller

--- src/main/java/com/example/demostudygroup/controllers/ViewController.java
+++ src/main/java/com/example/demostudygroup/controllers/ViewController.java
@@ -1,13 +1,19 @@
 package com.example.demostudygroup.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
 
 @Controller
 public class ViewController {
 
     @RequestMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("datetime",new Date());
+        model.addAttribute("username","Mluleki ");
+        model.addAttribute("mode","production");
         return "index";
     }
 }
